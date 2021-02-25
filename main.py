4c2dcd428ae888073bbbdda252bde39dafbe20f6
fededav <67575679+raishid@users.noreply.github.com>
2021-02-24 22:05:33 -0400

    final

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ url_curso = input('Introduce la url del curso: ')
 curso = udemy.Get_curso(url_curso)
 print('OBTENIENDO LOS CAPITULOS DEL CURSO')
 videos = udemy.Get_all_videos(curso)
+count = 1
 for capitulo in videos:
-    udemy.Descargar_video(curso, capitulo)
-
-
+    udemy.Descargar_video(curso, str(count)+' - '+capitulo)
+    count += 1
